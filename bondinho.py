97c1ed0f260b6b26f9e5b161957912fa1ba14f28
Denys Makene <denysmakene13@gmail.com>
2024-05-03 00:47:19 -0300

    Adiciona bondinho

---
+++ bondinho.py
@@ -0,0 +1,9 @@
+def bondinho():
+    soma = 0
+    for i in range(2):
+        soma += int(input())
+    if soma <= 50:
+        print('S')
+    else:
+        print('N')        
+bondinho()        
